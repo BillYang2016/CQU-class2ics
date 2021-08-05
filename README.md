4cc2faca13877bb23b7324782f0ccbc98e885564
Bill_Yang <ym13883407586@outlook.com>
2021-08-05 21:43:08 +0800

    adapt new enroll api

--- README.md
+++ README.md
@@ -32,10 +32,12 @@ ics 同时可以导入至 windows 日历以外的系统日历软件，包括但�
 如果您是重庆大学本校学生，请按照以下步骤进行使用：
 1. 登录`http://my.cqu.edu.cn/`
 2. 进入选课管理界面
-3. 在 `Network` 中选择`status`项，并将`Authorization`与`Cookie`填入`crawller.py`
-4. 运行`crawller.py`
-5. 运行`excelReader.py`
-6. 运行`main.py`
+3. 切换至 `Network` 栏，刷新页面
+4. 选择`register`栏，并将底部`Headers`中`Authorization`与`Cookie`填入`crawller.py`
+5. 将学号填入`crawller.py`
+6. 运行`crawller.py`
+7. 运行`excelReader.py`
+8. 运行`main.py`
 
 如果您不是重庆大学学生，请手动创建 Excel 文件，并执行 5、6 步（详情请见参考项目）
 
--- crawller.py
+++ crawller.py
@@ -6,6 +6,8 @@ import requests
 import json
 import xlwt
 
+stuid = '' # 请填入学号
+
 headers = {
 	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
 	'Host': 'my.cqu.edu.cn',
@@ -33,47 +35,38 @@ def getCourseTime(encryptedCourseId):  # 通过课程加密名称获取已选课
 		i += 1
 
 
-def printExcel(courseName, courseDetails):
+def printExcel(courseName, instructorName, courseDetails):
 	dic1 = {'一': 1, '二': 2, '三': 3, '四': 4, '五': 5, '六': 6, '日': 7}
-	timestr = courseDetails['classTime']
-	times = timestr.split(';')
+	timestr = courseDetails['teachingWeekFormat']
+	times = timestr.split(',')
 
-	global f, sheet1, row, classTimejson
+	global f, sheet1, row, classTimejson, errors
 
 	i = 0
 	while(i < len(times)):  # 对于每个课程时间单独处理
-		timeformat = times[i].split(' ')
-		if(len(timeformat) < 4):  # 格式有误，可能是未分配时间
-			print(courseName + '时间或地点未分配，已跳过处理')
-			i += 1
-			continue
 		row += 1
-		if(timeformat[0].find('-') != -1):
-			startWeek = timeformat[0].partition('周')[0].partition('-')[0]
-			endWeek = timeformat[0].partition('周')[0].partition('-')[2]
+		if(times[i].find('-') != -1):
+			startWeek = times[i].partition('-')[0]
+			endWeek = times[i].partition('-')[2]
 		else:
-			startWeek = endWeek = timeformat[0].partition('周')[0]
-		weekday = dic1[timeformat[1].partition('星期')[2]]
-		classroom = timeformat[3].partition('&')[2]
+			startWeek = endWeek = times[i]
+		weekday = dic1[courseDetails['weekDayFormat']]
+		classroom = courseDetails['roomName']
 		
 		j = 0
 		classTime = -1
 		while(j < len(classTimejson['classTime'])):  # 获取json中对应编号
-			if(timeformat[2] == classTimejson['classTime'][j]['name']):
+			if(courseDetails['periodFormat'] == classTimejson['classTime'][j]['name']):
 				classTime = j
 			j += 1
 
 		if(classTime == -1):
-			print('检测到未配置的课程时间：'+timeformat[2]+'，请在 conf_classTime.json 中进行配置')
+			print('检测到未配置的课程时间：' + courseDetails['periodFormat'] + '小节，请在 conf_classTime.json 中进行配置')
+			errors += 1
 		else:
 			classTime += 1
 
-		extraWeek = -1
-		if(endWeek.find(',') != -1):  # 有的时间分配带逗号，单独处理
-			extraWeek = endWeek.partition(',')[2]
-			endWeek = endWeek.partition(',')[0]
-
-		sheet1.write(row, 0, courseName + ' ' + courseDetails['instructorNames'])  # 课程名+导师名
+		sheet1.write(row, 0, courseName + ' ' + instructorName)  # 课程名 + 导师名
 		sheet1.write(row, 1, startWeek)  # 开始周
 		sheet1.write(row, 2, endWeek)  # 结束周
 		sheet1.write(row, 3, weekday)  # 周几
@@ -81,21 +74,10 @@ def printExcel(courseName, courseDetails):
 		sheet1.write(row, 5, classTime)  # 上课时间
 		sheet1.write(row, 6, classroom)  # 教室
 
-		if(extraWeek != -1):
-			row += 1
-			sheet1.write(row, 0, courseName + ' ' + courseDetails['instructorNames'])  # 课程名+导师名
-			sheet1.write(row, 1, extraWeek)  # 开始周
-			sheet1.write(row, 2, extraWeek)  # 结束周
-			sheet1.write(row, 3, weekday)  # 周几
-			sheet1.write(row, 4, 3)  # 单数双数周（暂无用）
-			sheet1.write(row, 5, classTime)  # 上课时间
-			sheet1.write(row, 6, classroom)  # 教室
-
 		i += 1
 
 
-def processDetails(courseName, courseDetails):
-	printExcel(courseName, courseDetails)  # 一般理论课
+def processDetails(courseDetails):
 	
 	if courseDetails['childrenList'] is not None:  # 有实验课
 		print('课程包含实验课，正在处理实验课信息')
@@ -103,15 +85,12 @@ def processDetails(courseName, courseDetails):
 
 
 if __name__ == '__main__':
-	url = 'http://my.cqu.edu.cn/enroll-api/enrollment/course-list?selectionSource=%E4%B8%BB%E4%BF%AE'
+	url = 'http://my.cqu.edu.cn/api/enrollment/timetable/student/' + stuid
 
 	body = requests.get(url, headers=headers)  # 完成假访问
 	courses = json.loads(body.text)
 
-	flag = 0
-	courseList = []
-
-	global f, sheet1, row, classTimejson
+	global f, sheet1, row, classTimejson, errors
 	f = xlwt.Workbook()
 	sheet1 = f.add_sheet(u'sheet1', cell_overwrite_ok=True)  # 创建Excel
 	row0 = [u'className', u'startWeek', u'endWeek', u'weekday', u'week', u'classTime', u'classroom']
@@ -123,17 +102,32 @@ if __name__ == '__main__':
 	with open(sys.path[0] + '/conf_classTime.json', 'r', encoding='UTF-8') as jsonfile:
 		classTimejson = json.load(jsonfile)
 
-	while(flag < 5):  # 主修、体育、英语、通识、非限
-		i = 0
-		while(i < len(courses['data'][flag]['courseVOList'])):
-			if(courses['data'][flag]['courseVOList'][i]['courseEnrollSign'] == '已选'):
-				courseList += courses['data'][flag]['courseVOList'][i]
-				print('正在获取课程 ' + courses['data'][flag]['courseVOList'][i]['name'] + ' 信息')
-				courseDetails = getCourseTime(courses['data'][flag]['courseVOList'][i]['encryptedCourseId'])
-				print('已获取导师名: ' + courseDetails['instructorNames'] + ' 与课程时间地点')
-				processDetails(courses['data'][flag]['courseVOList'][i]['name'], courseDetails)
-				print('已储存课程信息\n')
-			i += 1
-		flag += 1
+	i = 0
+	lasting = 1
+	lastID = 0
+	while(i < len(courses['data'])):
+		courseDetails = courses['data'][i]
+		courseName = courseDetails['courseName']
+		instructorName = courseDetails['classTimetableInstrVOList'][0]['instructorName']
+		courseID = courseDetails['id']
+		
+		if(courseDetails['classType'] != '理论'):
+			courseName = courseName + courseDetails['classType']
+		
+		if(lastID == courseID):
+			lasting += 1
+		else:
+			lasting = 1
+		
+		if(lasting == 1):
+			print('已获取课程 ' + courseName + ' 导师名: ' + instructorName + ' 与课程时间地点')
+		
+		printExcel(courseName, instructorName, courseDetails)  # 获取信息填充Excel
+		
+		if(lasting == body.text.count("\"id\":\"" + str(courseID) + "\"")):
+			print('已储存课程信息\n')
+		
+		i += 1
+		lastID = courseID
 
 	f.save('classInfo.xls')
